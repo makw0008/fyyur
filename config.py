a1cef7f59e5b8b20bf747a75fb9a99d5ba35ffab
makw0008 <makw0008@github.com>
2020-06-04 11:49:48 -0400

    database connection done sucessfully

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
-basedir = os.path.abspath(os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.dirname('C:\\Users\\makwa\\OneDrive\\Documents\\GitHub\\fyyur'))
 
 # Enable debug mode.
 DEBUG = True
@@ -10,4 +10,4 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost:5432/fyyur'
